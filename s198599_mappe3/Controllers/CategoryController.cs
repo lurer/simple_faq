8375d98b217cef6700f17375e34175f2fa5e3ebe
espenbz@gmail.com <espenbz@gmail.com>
2015-11-10 18:16:35 +0100

    Kommet ganske langt med front-end.

---
+++ s198599_mappe3/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using s198599_mappe3.Models.Domain;
+using s198599_mappe3.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace s198599_mappe3.Controllers
+{
+    public class CategoryController : ApiController
+    {
+
+        IFaqDb<Category> db = new CategoryRepo();
+
+
+        public HttpResponseMessage Get()
+        {
+            var liste = db.getList();
+            var Json = new JavaScriptSerializer();
+
+            string JsonString = Json.Serialize(liste);
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonString, Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ s198599_mappe3/Models/Repository/CategoryRepo.cs
@@ -0,0 +1,41 @@
+﻿using s198599_mappe3.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace s198599_mappe3.Models.Repository
+{
+    public class CategoryRepo : IFaqDb<Category>
+    {
+        DataContext context = new DataContext();
+
+        public bool delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool edit(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> getList()
+        {
+            return context.Category.Select(q => 
+            new Category {
+                CategoryID = q.CategoryID,
+                CategoryName = q.CategoryName }).ToList();
+        }
+
+        public bool save(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
